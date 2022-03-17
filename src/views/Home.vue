8139204b52117c1234b9726c78554f2b0ef87a51
objectMaker <528154320@qq.com>
2022-03-17 19:17:41 +0800

    '新增'

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,68 @@
 <template>
-  <div>{{ a }}</div>
+  <el-row class="outer_container">
+    <el-col :span="3">
+      <el-menu
+        active-text-color="#ffd04b"
+        background-color="#545c64"
+        class="el-menu-vertical-demo"
+        default-active="2"
+        text-color="#fff"
+        @open="handleOpen"
+        @close="handleClose"
+      >
+        <el-sub-menu index="1">
+          <template #title>
+            <el-icon><location /></el-icon>
+            <span>Navigator One</span>
+          </template>
+          <el-menu-item-group title="Group One">
+            <el-menu-item index="1-1">item one</el-menu-item>
+            <el-menu-item index="1-2">item one</el-menu-item>
+          </el-menu-item-group>
+          <el-sub-menu index="1-4">
+            <template #title>item four</template>
+            <el-menu-item index="1-4-1">item one</el-menu-item>
+          </el-sub-menu>
+        </el-sub-menu>
+        <el-menu-item index="2">
+          <el-icon><icon-menu /></el-icon>
+          <span>Navigator Two</span>
+        </el-menu-item>
+        <el-menu-item index="3" disabled>
+          <el-icon><document /></el-icon>
+          <span>Navigator Three</span>
+        </el-menu-item>
+        <el-menu-item index="4">
+          <el-icon><setting /></el-icon>
+          <span>Navigator Four</span>
+        </el-menu-item>
+      </el-menu>
+    </el-col>
+  </el-row>
 </template>
 <script setup>
 import axios from 'axios';
 async function getUserInfo() {
   const res = await axios.post('/api/user', {
     data: {
-      id: 2,
+      id: 2, //id为2是管理员
     },
   });
   console.log(res, 'res');
 }
 getUserInfo();
-import { ref } from 'vue';
-const a = ref(0);
+const handleOpen = (key, keyPath) => {
+  console.log(key, keyPath);
+};
+const handleClose = (key, keyPath) => {
+  console.log(key, keyPath);
+};
 </script>
-<style scoped lang="scss"></style>
+<style scoped>
+.outer_container {
+  height: 100vh;
+}
+.el-menu {
+  height: 100%;
+}
+</style>
